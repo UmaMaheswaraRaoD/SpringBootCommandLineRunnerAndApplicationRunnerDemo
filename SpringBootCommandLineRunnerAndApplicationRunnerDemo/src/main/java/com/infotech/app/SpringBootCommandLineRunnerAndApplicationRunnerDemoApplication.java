b25752fa60ad92f8491f8c3a66f1aadf4b8313f9
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-08-04 10:54:28 +0530

    code

---
+++ SpringBootCommandLineRunnerAndApplicationRunnerDemo/src/main/java/com/infotech/app/SpringBootCommandLineRunnerAndApplicationRunnerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.infotech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommandLineRunnerAndApplicationRunnerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommandLineRunnerAndApplicationRunnerDemoApplication.class, args);
+	}
+}
